59c0358e855e1d3175dd7101d7c1dedf19c54e26
Aur Saraf <sonoflilit@gmail.com>
2022-06-10 03:23:29 +0300

    units move

--- poly.py
+++ poly.py
@@ -1,4 +1,10 @@
 import pyglet
+import math
+
+
+def clamp(x, smallest, largest):
+    return max(smallest, min(x, largest))
+
 
 pyglet.resource.path = ["assets"]
 pyglet.resource.reindex()
@@ -17,23 +23,76 @@ for image in images.values():
 
 main_batch = pyglet.graphics.Batch()
 
-units = [
-    pyglet.sprite.Sprite(img=images["unit"], x=100 + 100 * i, y=100, batch=main_batch)
-    for i in range(4)
-]
+
+class UnitAI:
+    def __init__(self, unit) -> None:
+        self.unit = unit
+        self.unit.set_speed(1)
+
+    def update(self, dt):
+        self.unit.left(0.5 * dt)
+
+
+class Unit(pyglet.sprite.Sprite):
+    def __init__(self, x, y, batch):
+        self.velocity = 0.0
+        self.ai = UnitAI(self)
+
+        super().__init__(img=images["unit"], x=x, y=y, batch=batch, subpixel=True)
+
+    def _update_rotation(self, dr):
+        self.rotation += dr
+        r = -math.radians(self.rotation)
+        self.orientation = (math.sin(r), math.cos(r))
+
+    def right(self, dt):
+        self._update_rotation(-10 * dt)
+
+    def left(self, dt):
+        self._update_rotation(10 * dt)
+
+    def set_speed(self, speed):
+        self.velocity = 20.0 * clamp(speed, -0.5, 1.0)
+
+    def update(self, dt):
+        if not hasattr(self, "orientation"):
+            self._update_rotation(0.0)
+        x_part, y_part = self.orientation
+        self.x -= x_part * self.velocity * dt
+        self.y += y_part * self.velocity * dt
+
+
+def update(dt):
+    for obj in units:
+        obj.update(dt)
+
+
+def update_ai(dt):
+    for obj in units:
+        obj.ai.update(dt)
+
+
+units = [Unit(x=100 + 100 * i, y=100, batch=main_batch) for i in range(4)]
 
 turrets = [
-    pyglet.sprite.Sprite(img=images["turret"], x=100 + 200 * i, y=500, batch=main_batch)
+    pyglet.sprite.Sprite(img=images["turret"], x=100 + 200 * i, y=600, batch=main_batch)
     for i in range(3)
 ]
 
-game_window = pyglet.window.Window(800, 600)
+window = pyglet.window.Window(1024, 768)
 pyglet.gl.glClearColor(1, 1, 1, 1)
+pyglet.clock.schedule_interval(update, 1 / 120.0)
+pyglet.clock.schedule_interval(update_ai, 1 / 30.0)
 
 
-@game_window.event
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    print((x, y))
+
+
+@window.event
 def on_draw():
-    game_window.clear()
+    window.clear()
     main_batch.draw()
 
 
